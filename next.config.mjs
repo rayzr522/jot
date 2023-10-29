63de50912bb815629dec068a4c17cd11895eeeb7
rayzr522 <rayzr522@gmail.com>
2023-10-29 19:01:49 -0400

    disable lint & typecheck during next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,27 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-import "./src/env.mjs";
-
-const withPWA = withPWAInit({
-  dest: "public",
-});
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js"
+import "./src/env.mjs"
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-// export default withPWA(nextConfig);
-export default nextConfig;
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+    })
+    return withPWA(nextConfig)
+  }
+  return nextConfig
+}
+
+export default nextConfigFunction
